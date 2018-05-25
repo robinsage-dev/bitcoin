fd42eae50709a209251761bbf14e381d79d8108a
Jordan Baczuk <jbaczuk@robinsage.com>
2018-05-25 12:43:43 -0600

    testing address prefix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -243,7 +243,7 @@ public:
         // vSeeds.emplace_back("seed.testnet.bitcoin.sprovoost.nl");
         // vSeeds.emplace_back("testnet-seed.bluematt.me"); // Just a static list of stable node(s), only supports x9
 
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,107);//111);
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);//107);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,239);
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x35, 0x87, 0xCF};
