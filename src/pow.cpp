5a4363c38a5c3155e06a704ff2757ed458828ea2
Jordan Baczuk <jbaczuk@robinsage.com>
2018-06-06 22:55:33 -0600

    add getCompact to debug

--- src/pow.cpp
+++ src/pow.cpp
@@ -88,7 +88,7 @@ bool CheckProofOfWork(uint256 hash, unsigned int nBits, const Consensus::Params&
         return false;
 
     // Check proof of work matches claimed amount
-    LogPrintf("Claimed amount: %i Actual Amount: %i", bnTarget, UintToArith256(hash));
+    LogPrintf("Claimed amount: %i Actual Amount: %i", bnTarget.getCompact(), UintToArith256(hash).getCompact());
     if (UintToArith256(hash) > bnTarget)
         return false;
 
